1d1cd7aaa3f3708025d7dc440f5a2c9889254378
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-03-15 09:36:58 -0700

    added post route in tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,6 +35,14 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", async (req, res) => {
