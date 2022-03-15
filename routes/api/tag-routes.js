fb42841fd889c391091843a23a57c93e5181c787
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-03-15 09:38:04 -0700

    added put route in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,10 +21,10 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product}],
+      include: [{ model: Product }],
     });
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
+      res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
     res.status(200).json(tagData);
@@ -37,8 +37,8 @@ router.post("/", async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create({
-      tag_name: req.body.tag_name
-    })
+      tag_name: req.body.tag_name,
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -47,6 +47,19 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      { where: { id: req.params.id } }
+    );
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
